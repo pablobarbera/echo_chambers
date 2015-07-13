a36d8cb6258b8da605e6235a3b785b3b5a550c14
Pablo Barberá <pablo.barbera@nyu.edu>
2015-07-13 15:45:50 -0400

    revised code for Figure 1

--- 03_analysis/12-validation.r
+++ 03_analysis/12-validation.r
@@ -115,6 +115,35 @@ pq
 
 ggsave(filename="plots/figure1b.pdf", plot=pq, height=4, width=4)
 
+
+d$party <- factor(d$party)
+levels(d$party) <- c("Democrats", "Independents", "Republicans")
+
+p <- ggplot(d, aes(y=idealPoint, x=svd.phi, shape=party, color=party))
+pq <- p + geom_point(size=1.5, aes(shape=party, color=party)) +
+        scale_shape_manual(values=c(4, 0, 1)) +
+        scale_x_continuous("Estimated Twitter Ideal Points", limits=c(-1.7, 1.7), breaks=c(-1.5, 0, 1.5)) +
+        scale_y_continuous("Ideology Estimates Based on Roll-Call Votes\n(Clinton et al, 2004)", breaks=c(-2, -1, 0, 1, 2)) +
+        #scale_color_manual(values=c("blue", "darkgreen", "red")) +
+        theme(panel.border=element_rect(fill=NA), panel.background = element_blank()) 
+pq
+
+a1 <- data.frame(svd.phi = -1.4, idealPoint=-2.5, 
+            label=as.character(paste0("rho[D]==", c1)), party="Democrats")
+a2 <- data.frame(svd.phi = 1.3, idealPoint=2, 
+            label=as.character(paste0("rho[R]==", c2)), party="Republicans")
+a <- rbind(a1, a2)
+
+pq <- pq + annotate("text", y=a1$idealPoint, x=a1$svd.phi, label=as.character(a1$label), size=3, parse=TRUE) +
+            annotate("text", y=a2$idealPoint, x=a2$svd.phi, label=as.character(a2$label), size=3, parse=TRUE) +
+     geom_rangeframe(sides="bl", data=data.frame(idealPoint=c(-2, 2), svd.phi=c(-1.5, 1.5), party=NA), guide="none") + theme_tufte() +
+     theme(legend.position=c(0.18, 0.90),  legend.title=element_blank()) +
+    guides(colour = guide_legend(override.aes = list(linetype=c(0,0,0), shape=c(4,0,1), size=c(3,3,3))))
+
+pq
+
+ggsave(filename="plots/figure1b_revised.pdf", plot=pq, height=4, width=4)
+
 ######################################################################
 ### FIGURE 1(c): comparison with voting registration records
 ######################################################################
